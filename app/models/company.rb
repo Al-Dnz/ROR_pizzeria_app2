25ae5894487f4b184642673478bd432b231a6815
AlexDNZ <paleozene@gmail.com>
2024-02-16 14:59:35 +0100

    set main validations for Compamy model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+
+	validates :name, presence: true ,length: { minimum: 3 }
+	validates :name, uniqueness: true
+		{message: 'A pizzeria with name %{value} already exists'}
 end
