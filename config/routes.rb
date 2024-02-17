acf91eaf334893400b3dff8b4579b363839a6b7c
AlexDNZ <paleozene@gmail.com>
2024-02-17 13:00:40 +0100

    add endpoint to get available_years

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "home#index"
 
   get '/yearly_data/:year', to: 'companies#yearly_data'
+  get '/available_years', to: 'monthly_results#available_years'
+
 end
