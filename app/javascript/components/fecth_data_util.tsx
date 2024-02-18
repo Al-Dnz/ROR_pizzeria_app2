74cff6aa312ff4438154a79147fe76cb9220dd38
AlexDNZ <paleozene@gmail.com>
2024-02-18 15:56:43 +0100

    display specific message in thrown error

--- app/javascript/components/fecth_data_util.tsx
+++ app/javascript/components/fecth_data_util.tsx
@@ -2,7 +2,8 @@
 const fetchData = async (url: string, param: string = '') => {
 	const response = await fetch(`${url}/${param}`);
 	if (!response.ok) {
-		const message = `ERROR ${response.status} => ${response.statusText}`
+		const res = await response.json();
+		const message = `[ERROR ${response.status} => ${response.statusText}]: ${res.error? res.error : 'undefined error'}`
 	  	throw new Error(message);
 	}
 	const jsonData = await response.json();
