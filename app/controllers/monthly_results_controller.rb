c662fb2af99892a0a00f82f61edd13c839f4afa4
AlexDNZ <paleozene@gmail.com>
2024-02-18 15:55:37 +0100

    render json error when datas are not available

--- app/controllers/monthly_results_controller.rb
+++ app/controllers/monthly_results_controller.rb
@@ -2,6 +2,10 @@ class MonthlyResultsController < ApplicationController
 
 	def available_years
 		res = MonthlyResult.available_years
-		render json: res.to_json, status: :ok
+		if !res.empty? 
+			render json: res.to_json, status: :ok
+		else
+			render json: { error: "no datas available" }, status: :not_found
+		end
 	end
 end
